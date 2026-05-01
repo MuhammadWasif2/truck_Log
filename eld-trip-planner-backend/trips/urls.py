c19731e56b49b44f268cb3a4c2fd27121334faf0
MuhammadWasif2 <mwasif2002@gmail.com>
2026-05-01 05:49:20 +0500

    Routes fixes

--- eld-trip-planner-backend/trips/urls.py
+++ eld-trip-planner-backend/trips/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from .views import PlanTripView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path("plan-trip/", PlanTripView.as_view(), name="plan-trip"),
+    path("admin/", admin.site.urls),
+    path("api/", include("trips.urls")),
 ]
